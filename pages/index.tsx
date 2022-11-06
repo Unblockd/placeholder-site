774d10d92298c581de125afca9b821e5ca34056f
beauwilliams <beau.williams@icloud.com>
2022-11-06 15:42:19 +1100

    feaT: gh link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,58 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 // import Code from '../components/code'
 // <link rel="icon" href="/favicon.ico" />
 import { generateJSXMeshGradient } from "meshgrad";
 
-
 const Home: NextPage = () => {
   // Number of color stops
-const ELEMENTS = 5
+  const ELEMENTS = 5;
   return (
-  <>
+    <>
       <Head>
         <title>unblock.d - coming soon</title>
         <meta name="description" content="unblock.d - coming soon" />
       </Head>
-      <div style={generateJSXMeshGradient(ELEMENTS)} className='flex items-center justify-center h-screen w-screen'>
-        <div>
-
+      <div
+        style={generateJSXMeshGradient(ELEMENTS)}
+        className="flex items-center justify-center h-screen w-screen"
+      >
+        <div className="block p-6 space-y-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Your product{" "}
+            <mark className="px-2 text-black bg-yellow-200 rounded dark:bg-yellow-500">
+              unblock.d
+            </mark>
+          </h1>
+          <p className="text-lg font-normal text-black lg:text-xl dark:text-gray-400">
+            We build cool s**t that ends up in the hands of real people 🚀
+          </p>
+          <a
+            href="https://github.com/Unblockd"
+            className="w-full inline-flex justify-center items-center p-5 text-base font-medium text-gray-500 bg-gray-50 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <svg className="mr-3 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+            <span className="w-full">Reach us on GitHub</span>
+            <svg
+              aria-hidden="true"
+              className="ml-3 w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </a>
         </div>
-<a href="https://github.com/Unblockd" className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-<h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Your product <mark className="px-2 text-black bg-yellow-200 rounded dark:bg-yellow-500">unblock.d</mark></h1>
-<p className="text-lg font-normal text-black lg:text-xl dark:text-gray-400">We build cool s**t that ends up in the hands of real people 🚀</p>
-</a>
-    </div>
-      </>
+      </div>
+    </>
   );
 };
 
